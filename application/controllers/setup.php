93afc5b1b8046012e55d034ef152af3141dbe1e8
DominixZ <domesterz@gmail.com>
2009-10-12 22:38:10 +0700

    remove file

--- application/controllers/setup.php
+++ application/controllers/setup.php
@@ -1,9 +1,18 @@
 <?php
+
 class Setup extends DM_Controller {
+	
+	function Setup()
+	{
+		parent::DM_Controller();
+	}
 	
 	function index()
 	{
-	
+		echo "Hello World";
 	}
 	
 }
+
+/* End of file setup.php */
+/* Location: ./application/controllers/setup.php */
