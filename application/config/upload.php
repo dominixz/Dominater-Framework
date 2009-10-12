efeba454913266588e93d5156cfae090df604ec2
DominixZ <domesterz@gmail.com>
2009-10-13 00:51:23 +0700

    Add Folder

---
+++ application/config/upload.php
@@ -0,0 +1,7 @@
+<?php	 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['upload_path'] = './public/files/';
+$config['allowed_types'] = 'gif|jpg|png';
+$config['max_size']	= '100';
+$config['max_width']  = '1024';
+$config['max_height']  = '768';
