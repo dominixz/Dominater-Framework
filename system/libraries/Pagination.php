fc36314796d47fe852e46dcf0acc7871b36e295c
DominixZ <domesterz@gmail.com>
2009-10-15 23:56:35 +0700

    Fix Extend Helper bug and remove template.php from application/library

--- system/libraries/Pagination.php
+++ system/libraries/Pagination.php
@@ -237,6 +237,15 @@ class CI_Pagination {
 
 		return $output;
 	}
+	
+	function create_pagination($url='',$count=0,$offset=0,$local=TRUE)
+	{
+		$CI =& get_instance();
+		$this->base_url = $local ? $CI->config->site_url($url) : $url;
+		$this->total_rows = $count;
+		return $this->create_links();
+	}
+
 }
 // END Pagination Class
 
