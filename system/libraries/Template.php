4d333e6e875ae05b0488c870e3f58be722298502
DominixZ <domesterz@gmail.com>
2009-11-19 23:41:34 +0700

    Add function auto in Template Class

--- system/libraries/Template.php
+++ system/libraries/Template.php
@@ -2,25 +2,34 @@
 
 class Template {
 	var $template_data = array();
+	var $template_path;
 
+	function Template()
+	{
+		$this->CI =& get_instance();
+		$template = $this->CI->config->item('template');
+		$this->template_path = "{$template['dir']}{$template['default']}";
+	}
+	
 	function set($name, $value) {
 		$this->template_data[$name] = $value;
 	}
 
 	function load($template = '', $view = '' , $view_data = array(), $return = FALSE) {
-		$this->CI =& get_instance();
 		$this->set('contents', $this->CI->load->view($view, $view_data, TRUE));
 		return $this->CI->load->view($template, $this->template_data, $return);
 	}
 
 	// load a default template 'template.php'
-	function view($view = '' , $view_data = array(), $return = FALSE) {
-		$this->CI =& get_instance();
-		$template = $this->CI->config->item('template');
-		$template_path = "{$template['dir']}{$template['default']}";
-		
+	function view($view = '' , $view_data = array(), $return = FALSE) {	
 		$this->set('contents', $this->CI->load->view($view, $view_data, TRUE));
-		return $this->CI->load->view($template_path, $this->template_data, $return);
+		return $this->CI->load->view($this->template_path, $this->template_data, $return);
+	}
+	
+	function auto($view_data = array(), $return = FALSE) {	
+		$view = "{$this->CI->router->class}/{$this->CI->router->method}";
+		$this->set('contents', $this->CI->load->view($view, $view_data, TRUE));
+		return $this->CI->load->view($this->template_path, $this->template_data, $return);
 	}
 }
 
